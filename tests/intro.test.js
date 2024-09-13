048e3c0f06aee6868e299dceed61263c63bfef47
Damilola Osunsanwo <82976159+ddddami@users.noreply.github.com>
2024-09-13 20:52:49 +0100

    Exercise - Testing FixxBuzz

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,5 +1,5 @@
 import { it, expect, describe } from "vitest";
-import { max } from "../src/intro";
+import { fizzBuzz, max } from "../src/intro";
 
 describe("max", () => {
   it("should return first number if it is greater", () => {
@@ -12,5 +12,21 @@ describe("max", () => {
 
   it("should return first number if arguements are equal", () => {
     expect(max(2, 2)).toBe(2);
+  });
+});
+
+describe("fizzBuzz", () => {
+  it("should return Fizz if number is divisible by 3", () => {
+    expect(fizzBuzz(3)).toBe("Fizz");
+  });
+
+  it("should return Buzz if number is divisible by 5", () => {
+    expect(fizzBuzz(5)).toBe("Buzz");
+    expect(fizzBuzz(50)).toBe("Buzz");
+  });
+
+  it("should return number as a string if it is not divisible by 3 and 5", () => {
+    expect(fizzBuzz(1)).toBe("1");
+    expect(fizzBuzz(2)).toBe("2");
   });
 });
