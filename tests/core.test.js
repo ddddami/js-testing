807a66ed56265f05112af9a3e65e15341e9b66a1
Damilola Osunsanwo <82976159+ddddami@users.noreply.github.com>
2024-09-17 10:09:11 +0100

    Test validateUserInput

--- tests/core.test.js
+++ tests/core.test.js
@@ -1,5 +1,5 @@
 import { it, expect, describe } from "vitest";
-import { calculateDiscount, getCoupons } from "../src/core";
+import { calculateDiscount, getCoupons, validateUserInput } from "../src/core";
 
 describe("getCoupons", () => {
   it("should return an array of coupons", () => {
@@ -48,3 +48,40 @@ describe("calculateDiscount", () => {
     expect(calculateDiscount(10, 10)).toMatch(/invalid/i);
   });
 });
+
+describe("validateUserInput", () => {
+  it("should return successful if given valid input ", () => {
+    expect(validateUserInput("Damilola", 18)).toMatch(/success/i);
+  });
+
+  it("should return an error if username is not a string", () => {
+    expect(validateUserInput(10, 18)).toMatch(/invalid/i);
+  });
+
+  it("should return an error if username is less than 3 characters", () => {
+    expect(validateUserInput("ab", 18)).toMatch(/invalid/i);
+  });
+
+  it("should return an error if age is not a number", () => {
+    expect(validateUserInput("Dami", "18")).toMatch(/invalid/i);
+  });
+
+  it("should return an error if age is less than 18", () => {
+    expect(validateUserInput("Dami", 17)).toMatch(/invalid/i);
+  });
+  it("should return an error if inputs are invalid", () => {
+    expect(validateUserInput("", 17)).toMatch(/invalid username/i);
+    expect(validateUserInput("", 17)).toMatch(/invalid age/i);
+  });
+
+  it("should return an error if username is greater than 255", () => {
+    const username = "A".repeat(256);
+    expect(validateUserInput(username, 18)).toMatch(/invalid/i);
+  });
+
+  it("should return an error if age is greater than 100", () => {
+    expect(validateUserInput("username", 101)).toMatch(/invalid/i);
+  });
+});
+
+// Don't write unit tests based on our our functions are implemented. Because they may have bugs. Test with how the function should work in mind.
